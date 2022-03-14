4aef6418c3a797a3506b7c898f6ff0158cc2dca0
Guilherme Lara <guilherme.lara.sec@outlook.com>
2022-03-14 12:14:20 -0300

    Git Test VSCode

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
               <div class="inner">
                 <h3>150</h3>
 
-                <p>New Orders</p>
+                <p>Vendas</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
